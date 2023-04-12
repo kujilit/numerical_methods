8840d734293b388865dc0df843af1077fd82850d
Mike Litvin <mrlitvinm@gmail.com>
2023-04-12 13:57:17 +1000

    need to change method algorithm

--- Cholesky_decomposition/main.py
+++ Cholesky_decomposition/main.py
@@ -2,13 +2,15 @@ import numpy as np
 
 
 class Solution:
-    def __init__(self, matrix: np.array):
+    def __init__(self, matrix: np.array, b: np.array):
         self.dim = np.shape(matrix)[0]
         self.s_matrix = np.zeros((self.dim, self.dim))
 
         self.s_matrix[0][0] = np.sqrt(matrix[0][0])
 
         self.sum = 0
+        self.y = np.zeros((self.dim))
+        self.b = b
 
         for i in range(self.dim):
             for j in range(i + 1):
@@ -20,6 +22,10 @@ class Solution:
                 else:
                     self.s_matrix[i][j] = (1. / self.s_matrix[j][j] * (matrix[i][j] - self.sum))
 
+    def get_solution(self):
+        self.y = np.linalg.tensorsolve(self.s_matrix.T, self.b)
+        return np.linalg.tensorsolve(self.s_matrix, self.y)
+
     def display(self):
         print(self.s_matrix)
         print(np.matmul(self.s_matrix, self.s_matrix.transpose()))
@@ -27,9 +33,11 @@ class Solution:
 
 if __name__ == "__main__":
     a = np.array([
-        [4, 12, -16],
-        [12, 37, -43],
-        [-16, -43, 98]
+        [5.8, 0.3, -0.2],
+        [0.3, 4, -0.7],
+        [-0.2, -0.7, -6.7]
     ])
 
-    Solution(a).display()
+    b = np.array([3.1, -1.7, 1.1])
+
+    Solution(a, b).display()
